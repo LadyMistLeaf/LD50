5557502fcbda10a21461fbe4cac954f115f901ca
Tina Legere <30584144+tilegere@users.noreply.github.com>
2022-04-03 00:05:31 -0300

    Blurbs and Scroll
    Added temp images and work on the code block clicking and scroll functionality.

--- script/game.js
+++ script/game.js
@@ -1,16 +1,70 @@
-startGame = () => {
+let codeLocation = 0;
+let scrollY = 0;
+let mouseScroll = false;
+
+startGame = () => { //reInitializing all values so replayability is possible
     Game.state = "game";
+    // Making a copy of the screen data so the original stays intact for replayability
+    Game.data = JSON.parse(JSON.stringify(screenData)); 
+    codeLocation = SCREEN_TOP;
+    scrollY = 0;
+    mouseScroll = false;
 }
 
 drawGame = () => {
     gameCanvas.fillRect(0, 0, WIDTH, HEIGHT);
+    Game.data.forEach((codeBlock) => {
+        gameCanvas.drawImage(sprites[codeBlock.name], SCREEN_LEFT, codeLocation - scrollY * SCROLL_MULTIPLIER);
+        codeLocation += codeBlock.height;
+    });
+    codeLocation = SCREEN_TOP;
+    gameCanvas.drawImage(sprites.scroll, SCREEN_RIGHT, SCREEN_TOP + scrollY);
     gameCanvas.drawImage(sprites.main_bg, 0, 0);
+
 }
 
 gameMouseClick = (xCoord, yCoord) => {
-    if(xCoord >= SCREEN_LEFT && xCoord <= SCREEN_RIGHT){
+    if(xCoord >= SCREEN_LEFT && xCoord < SCREEN_RIGHT){
         if(yCoord >= SCREEN_TOP && yCoord <= SCREEN_BOTTOM){
-            console.log("Screen Clicked!");
+            let piece = null;
+            let index = 0;
+            let startY = SCREEN_TOP - scrollY * SCROLL_MULTIPLIER;
+            while(piece === null){
+                if(yCoord >= startY && yCoord <= startY + Game.data[index].height){
+                    piece = index;
+                }
+                else {
+                    startY += Game.data[index].height;
+                    index++;
+                }
+            }
+            console.log("Code block " + piece);
+        }
+    }
+    else if (xCoord >= SCREEN_RIGHT && xCoord <= SCREEN_RIGHT + SCROLL_WIDTH){
+        if(yCoord >= SCREEN_TOP + scrollY && yCoord <= SCREEN_TOP + scrollY + SCROLL_HEIGHT){
+            console.log("Mouse Scrolling!");
+            mouseScroll = true;
+        }
+    }
+}
+
+gameMouseUp = () => {
+    if(mouseScroll){
+        mouseScroll = false;
+    }
+}
+
+gameMouseMove = () => {
+    if(mouseScroll){
+        if(mouseY >= SCREEN_TOP && mouseY <= SCREEN_BOTTOM - SCROLL_HEIGHT){
+            scrollY = Math.round(mouseY - SCREEN_TOP);
+        }
+        else if(mouseY < SCREEN_TOP){
+            scrollY = 0;
+        }
+        else if(mouseY > SCREEN_BOTTOM - SCROLL_HEIGHT){
+            scrollY = SCREEN_BOTTOM - SCROLL_HEIGHT - SCREEN_TOP;
         }
     }
 }
--- script/spriteAltas.js
+++ script/spriteAltas.js
@@ -6,3 +6,9 @@ sprites.addImage = (imageName, imageSrc) => {
 }
 
 sprites.addImage("main_bg", "./assets/images/main_bg.png");
+
+
+// Temp images
+sprites.addImage("blurb1", "./assets/images/blurb1.png");
+sprites.addImage("blurb2", "./assets/images/blurb2.png");
+sprites.addImage("scroll", "./assets/images/scroll.png");
