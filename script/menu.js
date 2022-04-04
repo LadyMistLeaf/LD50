1f899ca824441d0413418a4cd9144cce3db55614
Tina Legere <30584144+tilegere@users.noreply.github.com>
2022-04-04 19:25:26 -0300

    Start Menu
    Start menu with start game button enabled

--- script/menu.js
+++ script/menu.js
@@ -2,15 +2,23 @@ let mouseX = 0;
 let mouseY = 0;
 
 drawMenu = () => {
-    gameCanvas.fillStyle = "#FFFFFF";
-    gameCanvas.fillRect(0, 0, WIDTH, HEIGHT);
-
-    gameCanvas.fillStyle = "#000000";
-    gameCanvas.fillText("Destructive Distractions!", 100, 100);
-
+    gameCanvas.drawImage(sprites.title_screen, 0, 0);
+    gameCanvas.drawImage(sprites.start, 391, 300);
+    gameCanvas.drawImage(sprites.how_to, 391, 360);
+    gameCanvas.drawImage(sprites.credits, 391, 420);
     gameCanvas.drawImage(sprites[mouseImage], mouseX - CURSOR_OFFSET_X, mouseY - CURSOR_OFFSET_Y);
 }
 
 menuMouseClick = (xCoord, yCoord) => {
-    startGame();
+    if(xCoord >= 391 && xCoord <= 634){
+        if(yCoord >= 300 && yCoord <= 335){
+            startGame();
+        }
+        else if(yCoord >= 360 && yCoord <= 395){
+            // How to
+        }
+        else if(yCoord >= 420 && yCoord <= 455){
+            // credits
+        }
+    }
 }
